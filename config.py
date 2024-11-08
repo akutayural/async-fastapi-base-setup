09d378344f7d9994031972ac3ec5be0b6698ecd7
Ahmet Kutay URAL <69294802+akutayural@users.noreply.github.com>
2024-11-08 05:51:10 +0000

    Response model of create user has been changed since users won't be able to see the user_id in the response.

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     # POSTGRESQL
     SQLALCHEMY_DATABASE_URL: str = (
-        f"postgresql+psycopg://kutay:6BO6AkEKxAMsgojspMuLXMonRc77S164@dpg-csmprh1u0jms73frf440-a/testdb_m72u"
+        f"xxxxxxxx"
     )
     REDIS_PORT: int = os.getenv("REDIS_PORT", 6379)
     REDIS_DB: int = os.getenv("REDIS_DB", 0)
