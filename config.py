bdac9865561371f28d3bbb356e24db074a40b29d
Ahmet Kutay URAL <69294802+akutayural@users.noreply.github.com>
2024-11-08 05:26:53 +0000

    requirements.txt change

--- config.py
+++ config.py
@@ -5,13 +5,8 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     # POSTGRESQL
-    POSTGRESQL_USER: str = os.getenv("POSTGRESQL_USER", "kutay")
-    POSTGRESQL_PASSWORD: str = os.getenv("POSTGRESQL_ROOT_PASSWORD", "123456")
-    POSTGRESQL_HOST: str = os.getenv("POSTGRESQL_HOST", "127.0.0.1")
-    POSTGRESQL_PORT: str = os.getenv("POSTGRESQL_PORT", "5432")
-    POSTGRESQL_DATABASE: str = os.getenv("POSTGRESQL_DATABASE", "testdb")
     SQLALCHEMY_DATABASE_URL: str = (
-        f"postgresql+psycopg://{POSTGRESQL_USER}:{POSTGRESQL_PASSWORD}@{POSTGRESQL_HOST}:{POSTGRESQL_PORT}/{POSTGRESQL_DATABASE}"
+        f"postgresql+asyncpg://postgresql://kutay:6BO6AkEKxAMsgojspMuLXMonRc77S164@dpg-csmprh1u0jms73frf440-a/testdb_m72u"
     )
     REDIS_PORT: int = os.getenv("REDIS_PORT", 6379)
     REDIS_DB: int = os.getenv("REDIS_DB", 0)
